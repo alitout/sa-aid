ba2b2207b8cb6bac7fc88ec6e2262412bef5d4ad
Ali Tout <alitout95@gmail.com>
2024-04-15 17:44:32 +0300

    edited organization abbreviation logic

--- backend/Models/organization/organizationModel.js
+++ backend/Models/organization/organizationModel.js
@@ -6,6 +6,11 @@ const organizationSchema = new mongoose.Schema({
         // required: true,
         unique: true
     },
+    OrganizationAbbreviation: {
+        type: String,
+        // required: true,
+        unique: true
+    },
     OrganizationName: {
         type: String,
         required: true,
@@ -28,5 +33,44 @@ organizationSchema.pre('save', async function (next) {
     }
     next();
 });
+
+organizationSchema.pre('save', async function (next) {
+    if (!this.isModified('OrganizationName')) {
+        return next();
+    }
+
+    const words = this.OrganizationName.split(' ');
+    let abbreviation = '';
+
+    if (words.length > 1) {
+        // Take the first letter from each word up to a maximum of 3 letters
+        abbreviation = words.slice(0, 3).map(word => word.charAt(0)).join('');
+    } else {
+        // Take the first 3 letters from the word
+        abbreviation = words[0].substring(0, 3);
+    }
+
+    abbreviation = abbreviation.toUpperCase();
+
+    // Check if the abbreviation already exists
+    const exists = await this.constructor.findOne({ OrganizationAbbreviation: abbreviation });
+
+    if (exists) {
+        let suffix = 1;
+
+        // Find a unique abbreviation
+        while (await this.constructor.findOne({ OrganizationAbbreviation: abbreviation + suffix })) {
+            suffix++;
+        }
+
+        abbreviation += suffix;
+    }
+
+    this.OrganizationAbbreviation = abbreviation;
+
+    next();
+});
+
+
 
 module.exports = mongoose.model('Organization', organizationSchema);
