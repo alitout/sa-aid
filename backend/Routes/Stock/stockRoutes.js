8f3f96989c8d9b125b41f0e2c8d0015892a03d39
Ali Tout <alitout95@gmail.com>
2024-04-22 16:24:20 +0300

    arranged the routes

--- backend/Routes/Stock/stockRoutes.js
+++ backend/Routes/Stock/stockRoutes.js
@@ -3,9 +3,9 @@ const stockController = require("../../Controllers/Stock/stockController");
 const router = express.Router();
 
 router.route("/api/stock/stock/create").post(stockController.createStock);
-router.route("/api/stock/stock/getAll").get(stockController.getAllStocks);
-router.route("/api/stock/stock/getById/:id").get(stockController.getStockById);
 router.route("/api/stock/stock/update/:id").patch(stockController.updateStockById);
 router.route("/api/stock/stock/delete/:id").delete(stockController.deleteStockById);
+router.route("/api/stock/stock/getAll").get(stockController.getAllStocks);
+router.route("/api/stock/stock/getById/:id").get(stockController.getStockById);
 
 module.exports = router;
