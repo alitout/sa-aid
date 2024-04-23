470058c4f25a3a2f96e7ff5e6022787a4be99308
Ali Tout <alitout95@gmail.com>
2024-04-23 17:57:36 +0300

    added homePage with initial routes

--- frontend/src/Routes/Routes.js
+++ frontend/src/Routes/Routes.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { useRoutes } from 'react-router-dom'
+import HomePage from '../Pages/homePage';
+
 
 const Routes = () => {
+    const AccessDenied = () => <div>You can't access this page</div>
     const routes = useRoutes([
-
+        // homePage
+        { path: '/', element: <HomePage /> },
     ]);
 
     return routes;
