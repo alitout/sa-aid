5eedc7ef478f7a7a17ba2ced797881dfe776fad8
Ali Tout <alitout95@gmail.com>
2024-04-23 15:40:39 +0300

    base for the project + bootstrap

---
+++ frontend/src/Routes/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+
+const Routes = () => {
+    const routes = useRoutes([
+
+    ]);
+
+    return routes;
+};
+
+export default Routes
