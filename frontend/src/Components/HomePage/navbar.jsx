6296af28c0b5895a1663b84968a7bf95c8e8faae
Ali Tout <alitout95@gmail.com>
2024-04-26 19:05:29 +0300

    added scroller to navbar

--- frontend/src/Components/HomePage/navbar.jsx
+++ frontend/src/Components/HomePage/navbar.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../../style/main.scss';
 import Logo from '../Logo/Logo';
 
 function Navbar() {
+
+    const topRef = useRef(null);
+    const scrollToTop = () => {
+        topRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    const scrollToSection = (section) => {
+        const element = document.getElementById(section);
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+
     return (
         <div className='px-0'>
             <nav className='navbar fixed-top d-sm-flex align-items-center justify-content-center justify-content-sm-between border-bottom gap-2 py-2 px-5'>
                 <div className="container justify-content-center justify-content-sm-between">
-                    <div className="logo col-md-2">
+                    <div className="logo col-md-2" id="logo" style={{ cursor: 'pointer' }} onClick={scrollToTop}>
                         <Logo />
                     </div>
                     <div className='navLinks d-none d-md-flex flex-row gap-3 col-6 justify-content-center align-items-center '>
-                        <a href='#a' className='navLink d-none d-sm-flex col-3 col-lg-2 justify-content-center text-center text-decoration-none text-black fw-500 py-4'>
+                        <a className='navLink d-none d-sm-flex col-3 col-lg-2 justify-content-center text-center text-decoration-none text-black fw-500 py-4'
+                            onClick={() => scrollToSection('objectives')} style={{ cursor: 'pointer' }}>
                             أهدافنا
                         </a>
-                        <a href='#b' className='navLink d-none d-sm-flex col-3 col-lg-2 justify-content-center text-center text-decoration-none text-black fw-500 py-4'>
+                        <a className='navLink d-none d-sm-flex col-3 col-lg-2 justify-content-center text-center text-decoration-none text-black fw-500 py-4'
+                            onClick={() => scrollToSection('security')} style={{ cursor: 'pointer' }}>
                             الحماية
                         </a>
-                        <a href='#c' className='navLink d-none d-sm-flex col-3 col-lg-2 justify-content-center text-center text-decoration-none text-black fw-500 p-4'>
+                        <a className='navLink d-none d-sm-flex col-3 col-lg-2 justify-content-center text-center text-decoration-none text-black fw-500 p-4'
+                            onClick={() => scrollToSection('advantages')} style={{ cursor: 'pointer' }}>
                             المميزات
                         </a>
                     </div>
@@ -25,8 +39,9 @@ function Navbar() {
                         <button className='button d-none d-sm-flex'>تسجيل الدخول</button>
                     </div>
                 </div>
-            </nav >
-        </div >
+            </nav>
+            <div ref={topRef} style={{ position: 'absolute', top: 0 }}></div>
+        </div>
     )
 }
 
