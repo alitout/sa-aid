ab3f40151e90afde787f29c05d367372f6f20c12
Ali Tout <alitout95@gmail.com>
2024-05-01 00:10:20 +0300

    added getSelf urls

--- frontend/src/externalApi/ExternalUrls.js
+++ frontend/src/externalApi/ExternalUrls.js
@@ -9,3 +9,7 @@ export const USER_LOGIN = 'http://localhost:3000/api/user/login';
 
 // register
 export const ORG_REGISTER = 'http://localhost:3000/api/organization/register';
+
+// getSelf
+export const ORG_GETSELF = 'http://localhost:3000/api/organization/getSelf';
+export const USER_GETSELF = 'http://localhost:3000/api/user/getSelf';
