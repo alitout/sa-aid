b8a1744769e3502acc5539dc6f41ec52d3745447
Ali Tout <alitout95@gmail.com>
2024-05-27 10:27:49 +0300

    added AddUsers api

--- frontend/src/externalApi/ExternalUrls.js
+++ frontend/src/externalApi/ExternalUrls.js
@@ -9,6 +9,7 @@ export const USER_LOGIN = 'http://localhost:3000/api/user/login';
 
 // register
 export const ORG_REGISTER = 'http://localhost:3000/api/organization/register';
+export const ORG_ADDUSER = 'http://localhost:3000/api/user/add';
 
 // getSelf
 export const ORG_GETSELF = 'http://localhost:3000/api/organization/getSelf';
